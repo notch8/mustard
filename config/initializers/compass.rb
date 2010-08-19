3439355b3b13ce63240a151fe2607f060ae117b2
Rob Kaufman <rob@notch8.com>
2010-08-19 09:50:30 -0700

    still working on styles

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,15 @@
 require 'compass'
 require 'compass/app_integration/rails'
 Compass::AppIntegration::Rails.initialize!
+
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+Sass::Plugin.on_updating_stylesheet do |template, css|
+  puts "Compiling #{template} to #{css}"
+end
+
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+                                             :urls => ['/stylesheets'],
+                                             :root => "#{Rails.root}/tmp")
+
