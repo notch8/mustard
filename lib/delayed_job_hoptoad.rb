2163fd9b331470536640c65c39908ff7b5e566fe
Rob Kaufman <rob@notch8.com>
2010-08-19 10:53:31 -0700

    added name to accounts

---
+++ lib/delayed_job_hoptoad.rb
@@ -0,0 +1,38 @@
+# begin
+ 
+  Delayed::Worker.logger.info "Adding Hoptoad support to Delayed::Job"
+  
+  begin
+    module Delayed
+      class Job
+        
+        def invoke_job_with_hoptoad
+          if defined?(HoptoadNotifier)
+            Delayed::Job.send(:define_method, :invoke_job_with_hoptoad) do
+              begin
+                invoke_job_without_hoptoad
+              rescue Exception => e
+                HoptoadNotifier.notify(e, :cgi_data => self.attributes)
+                raise e
+              end
+            end
+          else
+            Delayed::Job.send(:define_method, :invoke_job_with_hoptoad) do
+              invoke_job_without_hoptoad
+            end
+          end
+          invoke_job_with_hoptoad
+        end
+        
+        alias_method_chain :invoke_job, :hoptoad
+        
+      end # Job
+    end # Delayed
+  rescue Exception => e
+    Delayed::Worker.logger.error(e)
+    raise e
+  end
+ 
+  
+# rescue Exception => e
+# end
